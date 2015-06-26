6e6637e6019d57967e48615db432ab5c7b98e5e5
danielginn <daniel.ginn@live.com.au>
2015-06-26 15:17:56 +1000

    Fully working converter

--- main.cpp
+++ main.cpp
@@ -29,27 +29,22 @@ int main(int argc, char** argv)
 	VRmImage* p_target_img1 = 0;
 	VRmImage* p_target_img2 = 0;
 	VRmImage* p_target_img3 = 0;
+	VRmImage* p_check_img = 0;
 
-	VRmImageFormat source_format;
+
 	VRmImageFormat target_format;
 
-	//source_format.m_width = 1280;
-	//source_format.m_height = 960;
-	//source_format.m_color_format = BAYER_GRGB_8;
-	//source_format.m_image_modifier = 0;
+	target_format.m_width = 1280;
+	target_format.m_height = 960;
+	target_format.m_color_format = VRM_BGR_3X8;
+	target_format.m_image_modifier = 0;
 
-	//target_format.m_width = 1280;
-	//target_format.m_height = 960;
-	//target_format.m_color_format = VRM_ARGB_4X8;
-	//target_format.m_image_modifier = 0;
-
-	int i = 0;
-	sprintf(loadFileName1, "../Pictures/Flight Test 1/Raw/image_00%da.png",i);
-	sprintf(loadFileName2, "../Pictures/Flight Test 1/Raw/image_00%db.png",i);
-	sprintf(loadFileName3, "../Pictures/Flight Test 1/Raw/image_00%dc.png",i);
-	sprintf(saveFileName1, "../Pictures/Flight Test 1/Converted/image_00%da.png",i);
-	sprintf(saveFileName2, "../Pictures/Flight Test 1/Converted/image_00%db.png",i);
-	sprintf(saveFileName3, "../Pictures/Flight Test 1/Converted/image_00%dc.png",i);
+	sprintf(loadFileName1, "../Pictures/Flight Test 2/Raw/image_030a.png");
+	sprintf(loadFileName2, "../Pictures/Flight Test 2/Raw/image_030b.png");
+	sprintf(loadFileName3, "../Pictures/Flight Test 2/Raw/image_030c.png");
+	sprintf(saveFileName1, "../Pictures/Flight Test 2/Converted/image_030a.png");
+	sprintf(saveFileName2, "../Pictures/Flight Test 2/Converted/image_030b.png");
+	sprintf(saveFileName3, "../Pictures/Flight Test 2/Converted/image_030c.png");
 
 	// Load Images
 	cout << "Loading Images" << endl;
@@ -59,22 +54,64 @@ int main(int argc, char** argv)
 
 	cout << "Images loaded:\nWidth: " << p_source_img1->m_image_format.m_width << endl;
 	cout << "Height: " << p_source_img1->m_image_format.m_height << endl;
-	const char *target_color_format_str;
-	if (!VRmUsbCamGetStringFromColorFormat(p_source_img1->m_image_format.m_color_format,&target_color_format_str))
+	const char *source_color_format_str;
+	if (!VRmUsbCamGetStringFromColorFormat(p_source_img1->m_image_format.m_color_format,&source_color_format_str))
 		return 0;
-	cout << "Format: " << target_color_format_str << endl;
+	cout << "Format: " << source_color_format_str << endl;
+
+
+	//Wrap target image
+	VRM_EXEC_AND_CHECK(VRmUsbCamNewImage(&p_target_img1, target_format));
+	VRM_EXEC_AND_CHECK(VRmUsbCamNewImage(&p_target_img2, target_format));
+	VRM_EXEC_AND_CHECK(VRmUsbCamNewImage(&p_target_img3, target_format));
+
+
+	VRmDWORD target_list;
+	VRM_EXEC_AND_CHECK(VRmUsbCamGetTargetFormatListSize(&(p_source_img1->m_image_format), &target_list));
+	cout << "Target Format list size: " << target_list << endl;
+	cout << "Possible Target Formats:" << endl;
+	VRmDWORD j;
+	for (j=0;j<target_list;j++)
+	{
+		VRmImageFormat possible_format;
+		const char *target_color_format_str;
+		VRM_EXEC_AND_CHECK(VRmUsbCamGetTargetFormatListEntry(&(p_source_img1->m_image_format), j, &possible_format));
+		VRM_EXEC_AND_CHECK(VRmUsbCamGetStringFromColorFormat(possible_format.m_color_format,&target_color_format_str));
+		cout << target_color_format_str << endl;
+	}
 
 	// Target Image format
+	cout << "Selected target format:" << endl;
+	cout << "Width: " << p_target_img1->m_image_format.m_width << endl;
+	cout << "Height: " << p_target_img1->m_image_format.m_height << endl;
+	const char *target_color_format_str;
+	if (!VRmUsbCamGetStringFromColorFormat(p_target_img1->m_image_format.m_color_format,&target_color_format_str))
+		return 0;
+	cout << "Format: " << target_color_format_str << endl << endl;
 
+	cout << "Converting Images:" << endl;
 	// Convert Images
-	//VRM_EXEC_AND_CHECK(VRmUsbCamConvertImage(p_source_img1,p_target_img1));
-	//VRM_EXEC_AND_CHECK(VRmUsbCamConvertImage(p_source_img2,p_target_img2));
-	//VRM_EXEC_AND_CHECK(VRmUsbCamConvertImage(p_source_img3,p_target_img3));
+	VRM_EXEC_AND_CHECK(VRmUsbCamConvertImage(p_source_img1,p_target_img1));
+	VRM_EXEC_AND_CHECK(VRmUsbCamConvertImage(p_source_img2,p_target_img2));
+	VRM_EXEC_AND_CHECK(VRmUsbCamConvertImage(p_source_img3,p_target_img3));
 
+	cout << "Saving Images" << endl;
 	// Save Images
-	//VRM_EXEC_AND_CHECK(VRmUsbCamSavePNG(saveFileName1, p_target_img1, 0));
-	//VRM_EXEC_AND_CHECK(VRmUsbCamSavePNG(saveFileName2, p_target_img2, 0));
-	//VRM_EXEC_AND_CHECK(VRmUsbCamSavePNG(saveFileName3, p_target_img3, 0));
+	VRM_EXEC_AND_CHECK(VRmUsbCamSavePNG(saveFileName1, p_target_img1, 0));
+	VRM_EXEC_AND_CHECK(VRmUsbCamSavePNG(saveFileName2, p_target_img2, 0));
+	VRM_EXEC_AND_CHECK(VRmUsbCamSavePNG(saveFileName3, p_target_img3, 0));
+
+	//Unlocking target wraps
+	VRM_EXEC_AND_CHECK(VRmUsbCamFreeImage(&p_target_img1));
+	VRM_EXEC_AND_CHECK(VRmUsbCamFreeImage(&p_target_img2));
+	VRM_EXEC_AND_CHECK(VRmUsbCamFreeImage(&p_target_img3));
+	
+	cout << "Saved image was in file format: ";
+	VRM_EXEC_AND_CHECK(VRmUsbCamLoadPNG(saveFileName1, &p_check_img));
+	const char *check_color_format_str;
+	if (!VRmUsbCamGetStringFromColorFormat(p_check_img->m_image_format.m_color_format,&check_color_format_str))
+		return 0;
+	cout << check_color_format_str << endl;
 
 	return 0;
 }
